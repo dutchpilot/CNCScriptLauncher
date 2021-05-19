9d84c1bdfde2374ff4cda30a5206b410e32b448a
ivan_714 <l1ft@yandex.ru>
2021-05-19 15:07:49 +0400

    Minor code formatting.

--- Unit1.pas
+++ Unit1.pas
@@ -8,8 +8,8 @@ uses
 
 type
   TForm1 = class(TForm)
-    Edit1: TEdit;
-    Button2: TButton;
+    Barcode: TEdit;
+    ButtonClear: TButton;
     LabelSimpleStatus: TLabel;
     Panel1: TPanel;
     ListBox1: TListBox;
@@ -22,27 +22,27 @@ type
     EditPath: TEdit;
     Label1: TLabel;
     Label2: TLabel;
-    CheckBox1: TCheckBox;
-    Panel2: TPanel;
+    CheckBoxOpenMorbidelliEditor: TCheckBox;
+    PanelDetailCount: TPanel;
     RadioButton1: TRadioButton;
     RadioButton2: TRadioButton;
     Label3: TLabel;
-    procedure Edit1Change(Sender: TObject);
+    procedure BarcodeChange(Sender: TObject);
     procedure ButtonGenerateClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ClearScriptsClick(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure ButtonClearClick(Sender: TObject);
     procedure ButtonShowHideClick(Sender: TObject);
     procedure FormActivate(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure FormDeactivate(Sender: TObject);
     procedure EditPathMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
-    procedure Edit1MouseDown(Sender: TObject; Button: TMouseButton;
+    procedure BarcodeMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure ListBox1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
-    procedure CheckBox1Click(Sender: TObject);
+    procedure CheckBoxOpenMorbidelliEditorClick(Sender: TObject);
     procedure RadioButton1Click(Sender: TObject);
     procedure RadioButton2Click(Sender: TObject);
     protected Procedure LastFocus(var Mess : TMessage);
@@ -65,10 +65,11 @@ implementation
 
 Procedure TForm1.LastFocus(var Mess : TMessage);
 Begin
-  IF  Mess.wParam = WA_INACTIVE Then
-    Edit1.Color := clWindow
-  Else
-    Edit1.Color := clYellow;
+
+  if  Mess.wParam = WA_INACTIVE Then
+    Barcode.Color := clWindow
+  else
+    Barcode.Color := clYellow;
 
   if Mess.Msg =WM_SYSCOMMAND then
     if Mess.WParam=SC_MINIMIZE then
@@ -76,9 +77,10 @@ Begin
       Application.Restore;
     end;
 
-  Edit1.SetFocus;
+  Barcode.SetFocus;
 
   Inherited;
+
 End;
 
 procedure ManageFormDesign;
@@ -101,47 +103,50 @@ begin
     Form1.AlphaBlend := True;
     Form1.ButtonShowHide.Caption := 'Show';
   end;
+
 end;
 
-procedure TForm1.Edit1Change(Sender: TObject);
+procedure TForm1.BarcodeChange(Sender: TObject);
 var f, f_script: TextFile;
   Exist, LeadingZeros: Boolean;
   i: byte;
   detail, DetailName, FileResultName, path, edit, scriptPath, st_script, st: String;
+
 begin
+
   //Reading control commands 11841, 321321 or 654654
-  if StrPos('*', PChar(Edit1.Text)) <> nil then
-    Edit1.Text := ''
-  else if Edit1.Text = '11841' then
+  if StrPos('*', PChar(Barcode.Text)) <> nil then
+    Barcode.Text := ''
+  else if Barcode.Text = '11841' then
   begin
     FULLSTATE := not FULLSTATE;
-    Edit1.Text := '';
+    Barcode.Text := '';
   end
-  else if Edit1.Text = '-' then
+  else if Barcode.Text = '-' then
   begin
     Form1.ButtonGenerate.Click();
-    //ShellExecute(0, 'Open', 'GenerateScripts.exe', nil, nil, 1);
-    Edit1.Text := '';
+    Barcode.Text := '';
     LabelSimpleStatus.Caption := 'Generate at ' + TimeToStr(Time);
     Exit;
   end
-  else if Edit1.Text = '654654' then
+  else if Barcode.Text = '654654' then
   begin
     Form1.Close;
   end;
 
-  ManageFormDesign;//Set full or comapct application interface
+  //Set full or comapct application interface
+  ManageFormDesign;
 
-  if CheckBox1.Checked then
+  if CheckBoxOpenMorbidelliEditor.Checked then
     FileResultName := 'Scripts\result-editor.txt'
   else
     FileResultName := 'Scripts\result.txt';
 
   if FileExists(FileResultName) then
   BEGIN
-    edit := Edit1.Text;
+    edit := Barcode.Text;
 
-    if (length(Edit1.Text) = 13) and (Edit1.Text[1] = PREFIX) then
+    if (length(Barcode.Text) = 13) and (Barcode.Text[1] = PREFIX) then
     //Entering detail barcode
       begin
         try
@@ -162,9 +167,6 @@ begin
             end;
           end;
           edit := st + '+';
-          //ShowMessage(edit);
-          //edit := IntToStr(StrToInt(copy(edit, 3, 11)) );
-          //edit := edit + '+';
         except
         end;
       end;
@@ -189,7 +191,7 @@ begin
          if detail = copy(edit, 1, length(edit) - 1) then
          begin
             Exist := True;
-            if form1.CheckBox1.Checked then
+            if form1.CheckBoxOpenMorbidelliEditor.Checked then
             begin
               DetailName := detail + '-editor.ahk';
             end
@@ -214,22 +216,22 @@ begin
             EditPath.Text := st_script;
             CloseFile(f_script);
 
-            Edit1.Text := '';
+            Barcode.Text := '';
          end;
       end;
 
       CloseFile(f);
     end;
 
-    if (not Exist) and (Edit1.Text <> '') then
+    if (not Exist) and (Barcode.Text <> '') then
     begin
       LabelStatus.Caption := 'File ' + DetailName + ' NOT FOUND';
       LabelSimpleStatus.Caption := 'Code ' + DetailName + ' not found';
       EditPath.Text := '';
     end;
 
-    if length(Edit1.Text) >= 13 then
-      Edit1.Text := '';
+    if length(Barcode.Text) >= 13 then
+      Barcode.Text := '';
   END
   else
     LabelSimpleStatus.Caption := 'results.txt not found';
@@ -240,6 +242,7 @@ end;
 procedure GenerateScript(detail, path: String);
 var f1, f2, f_template1, f_template2: TextFile;
   st: String;
+
 begin
   AssignFile(f1, 'Scripts\' + detail + '-editor.ahk');
   AssignFile(f2, 'Scripts\' + detail + '.ahk');
@@ -294,6 +297,7 @@ end;
 
 procedure GetFiles(const dir: string; list: TStrings);
 var rec: TSearchRec;
+
 begin
   if FindFirst(dir + '\*.*', faAnyFile, rec) = 0 then
   repeat
@@ -318,10 +322,11 @@ var files: TStringList;
   i, k, j, st_start,st_end, details_counter: integer;
   f: TextFile;
   st, filename, detail, swap: String;
+
 begin
   details_counter := 0;
 
-  if Form1.CheckBox1.Checked then
+  if Form1.CheckBoxOpenMorbidelliEditor.Checked then
     AssignFile(f, 'Scripts\result-editor.txt')
   else
     AssignFile(f, 'Scripts\result.txt');
@@ -334,13 +339,12 @@ begin
 
   GetFiles(ROOTPATH, files);
   ListBox1.Clear;
-  //ListBox1.AddItem('Processed the following files in ' + ROOTPATH, Sender);
   for i := 0 to files.Count - 1 do
   begin
     st := files[i];
     k := length(st);
 
-    //> Get filename without file path and extension
+    //----> Get filename without file path and extension
     while (st[k] <> '.')and(k > 0) do
     begin
       dec(k);
@@ -353,7 +357,7 @@ begin
     end;
     st_start := k + 1;
     filename := copy(st, st_start, st_end - st_start);
-    //<
+    //<----
 
     //This code simply works as follows:
     //IN
@@ -389,25 +393,19 @@ begin
     end;
   end;
 
-  {for i := 0 to dirs.Count - 1 do
-  begin
-  ListBox1.AddItem(dirs[i], Sender);
-   //CopyFile('shortcut.lnk', PChar(dirs[i] + '\root.lnk'), false);
-    DeleteFile(PChar(dirs[i] + '/root.lnk'));
-  end;}
-
   files.Free;
   dirs.Free;
   CloseFile(f);
   Form1.LabelStatus.Caption := IntToStr(fileCount) + ' files in ' + intToStr(dirCount) + ' folders were processed (' + IntToStr(details_counter) + ' scripts created)';
 
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 var f: TextFile;
   st: String;
+
 begin
   AssignFile(f, 'config.ahk');
   Reset(f);
@@ -425,20 +423,19 @@ begin
     ListBox1.AddItem(st, Sender);
   if st = ';YES' then
   begin
-    CheckBox1.Visible := True;
+    CheckBoxOpenMorbidelliEditor.Visible := True;
     FEW_SCRIPTS := True;
   end;
 
   FULLSTATE := False;
   ManageFormDesign;
   CloseFile(f);
-  //CopyFile('shortcut.lnk', PChar(EditRoot.Text + '\root.lnk'), false);
-  //DeleteFile(PChar(EditRoot.Text + '\shortcut.lnk'));
 end;
 
 procedure TForm1.ClearScriptsClick(Sender: TObject);
 var files: TStringList;
   i:integer;
+
 begin
   files := TStringList.Create;
   GetFiles(GetCurrentDir + '\Scripts', files);
@@ -453,32 +450,31 @@ begin
 
   files.Free;
 
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+procedure TForm1.ButtonClearClick(Sender: TObject);
 begin
   LabelStatus.Caption := '';
-  Edit1.Text := '';
-  LabelStatus.Caption := '';
   ListBox1.Clear;
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Text := '';
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 procedure TForm1.ButtonShowHideClick(Sender: TObject);
 begin
   FULLSTATE := not FULLSTATE;
-  Edit1.Text := '';
   ManageFormDesign;
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Text := '';
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 procedure TForm1.FormActivate(Sender: TObject);
 begin
-  Edit1.Color := clYellow;
+  Barcode.Color := clYellow;
 end;
 
 procedure TForm1.Button3Click(Sender: TObject);
@@ -488,44 +484,44 @@ end;
 
 procedure TForm1.FormDeactivate(Sender: TObject);
 begin
-  Edit1.Color := clWindow;
+  Barcode.Color := clWindow;
 end;
 
 procedure TForm1.EditPathMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  Edit1.Color := clWindow;
+  Barcode.Color := clWindow;
 end;
 
-procedure TForm1.Edit1MouseDown(Sender: TObject; Button: TMouseButton;
+procedure TForm1.BarcodeMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  Edit1.Color := clYellow;
+  Barcode.Color := clYellow;
 end;
 
 procedure TForm1.ListBox1MouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
-procedure TForm1.CheckBox1Click(Sender: TObject);
+procedure TForm1.CheckBoxOpenMorbidelliEditorClick(Sender: TObject);
 begin
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 procedure TForm1.RadioButton1Click(Sender: TObject);
 begin
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 procedure TForm1.RadioButton2Click(Sender: TObject);
 begin
-  Edit1.Color := clYellow;
-  Edit1.SetFocus;
+  Barcode.Color := clYellow;
+  Barcode.SetFocus;
 end;
 
 end.
